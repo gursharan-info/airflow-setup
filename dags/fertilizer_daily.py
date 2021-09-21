e42dd62f69a6a4238635df16ba8a275dbb2b8909
Gursharan Singh Dhanjal <gursharan.info@gmail.com>
2021-09-21 17:41:32 +0530

    Corrected historical mean for fetrilizer daily

--- dags/fertilizer_daily.py
+++ dags/fertilizer_daily.py
@@ -8,121 +8,122 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from helpers import google_upload as gupload
 
-# lgd_codes_file = 'https://raw.githubusercontent.com/gursharan-info/idp-scripts/master/sources/LGD_v1_17Oct19.csv'
+lgd_codes_file = 'https://raw.githubusercontent.com/gursharan-info/idp-scripts/master/sources/LGD_v2_17Sep21_fertilizer.csv'
 dir_path = '/usr/local/airflow/data/hfi/fertilizer_sales'
 data_path = os.path.join(dir_path, 'daily')
+raw_data_path = os.path.join(dir_path, 'raw_data')
 gdrive_fertilizer_folder = '1EZeIWEq_Yshb-C-0E1HBXzBDh5luPsZf'
-day_lag = 6
+gdrive_fertilizer_raw_folder = '1zgBIGhN4j0TDI0HdMqlxaTebb0iIgwPo'
+day_lag = 2
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def read_fertilizer_data(**context):
     # Load the main page
+    # The current date would be previous day from date of execution
+    curr_date = datetime.fromtimestamp(context['execution_date'].timestamp())- timedelta(day_lag)
+    print(curr_date)
+
     try:
         resp = requests.get('https://reports.dbtfert.nic.in/mfmsReports/getPOSReportForm', verify = False)
-        # , headers=headers)#, proxies=proxies)
         main_content = resp.content
-        main_soup = BeautifulSoup(main_content, 'html.parser') 
-        # print(context['execution_date'], type(context['execution_date']))
-        
-        # The current date would be previous day from date of execution
-        currentDate = datetime.fromtimestamp(context['execution_date'].timestamp())- timedelta(day_lag)
-        print(currentDate)
-        # currentDate = datetime.today()
-         
-        # Get list of states from the response
+        main_soup = BeautifulSoup(main_content, 'html.parser')
         states = [option['value'] for option in main_soup.find("select",{"id":"parameterStateName"}).findAll("option")[1:] ]
 
-        state_codes_df = pd.read_csv(os.path.join(dir_path, 'LGD_v1_17Oct19_fertilizer.csv'))
-        state_codes_df.rename(columns={'D_NAME': 'District_lgd', 'D_CODE': 'District_code', 'S_NAME': 'State_lgd', 'S_CODE': 'State_code'}, inplace=True)
-        state_codes_df[['District_lgd','State_lgd']] = state_codes_df[['District_lgd','State_lgd']].apply(lambda x: x.str.strip().str.upper())
-        state_codes_df['state_district_lower'] = state_codes_df['State_lgd'].str.lower() + "_" + state_codes_df['District_lgd'].str.lower()
-
-        all_data = []
-        # Fetch discricts and relevant data for each state
+        state_dist_list = []
         for state in states:
             dist_url = "https://reports.dbtfert.nic.in/mfmsReports/getDistrictList"
             state_resp = requests.post(url = dist_url, data = {'selectedStateName': state}, verify = False)
-               
             districts = list( json.loads(state_resp.text).keys() )
             # print(state, districts)
             for dist in districts:
-                f_url = "https://reports.dbtfert.nic.in/mfmsReports/getPOSReportFormList.action"
-                form_data = { "parameterStateName": state, "parameterDistrictName": dist,
-                            "parameterFromDate": currentDate.strftime("%d/%m/%Y"),
-                            "parameterToDate": currentDate.strftime("%d/%m/%Y")
-                            }
-                state_dist_resp = requests.post(url = f_url, data = form_data, verify = False) 
-                state_dist_resp_cookies = state_dist_resp.cookies
-                
-                report_url = "https://reports.dbtfert.nic.in/mfmsReports/report.jsp"
-                report_resp = requests.post(url = report_url, verify = False, cookies=state_dist_resp_cookies) 
+                state_dist_list.append(dict(state_name=state, dist_name=dist))
 
-                df = pd.read_csv(io.StringIO(report_resp.content.decode('utf-8')), header=1).reset_index(drop=True).iloc[:-1,:].iloc[:, :-1]
-                df = df.drop(df.columns[0],axis=1)
-                df.insert(0, 'State_Name', state)
-                df.insert(0, 'Date', currentDate.strftime("%d-%m-%Y"))
-                all_data.append(df)
+        # state_codes_df = pd.read_csv(os.path.join(dir_path, 'LGD_v2_17Sep21_fertilizer.csv'))
+        state_codes_df = pd.read_csv(lgd_codes_file)
+        state_codes_df[['district_name','state_name']] = state_codes_df[['district_name','state_name']].apply(lambda x: x.str.strip().str.upper())
+        state_codes_df['merge_name'] = state_codes_df['state_fert'].str.strip().str.lower() + "_" + state_codes_df['district_fert'].str.strip().str.lower()
+        
+        all_data = []
+        for state_dist in state_dist_list:
+#             print(state_dist)
+            f_url = "https://reports.dbtfert.nic.in/mfmsReports/getPOSReportFormList.action"
 
+            form_data = { "parameterStateName": state_dist['state_name'], "parameterDistrictName": state_dist['dist_name'],
+                                "parameterFromDate": curr_date.strftime("%d/%m/%Y"),
+                                "parameterToDate": curr_date.strftime("%d/%m/%Y")
+                                }        
+            state_dist_resp = requests.post(url = f_url, data = form_data, verify = False) 
+
+            state_dist_resp_cookies = state_dist_resp.cookies
+
+            report_url = "https://reports.dbtfert.nic.in/mfmsReports/report.jsp"
+            report_resp = requests.post(url = report_url, verify = False, cookies=state_dist_resp_cookies) 
+
+            df = pd.read_csv(io.StringIO(report_resp.content.decode('utf-8')), header=1).reset_index(drop=True).iloc[:-1,:].iloc[:, :-1]
+            df = df.drop(df.columns[0],axis=1)
+            df.insert(0, 'State_Name', state_dist['state_name'])
+            df.insert(0, 'Date', curr_date.strftime("%d-%m-%Y"))
+
+            all_data.append(df)
+        
         all_state_data = pd.concat(all_data).drop_duplicates()
+        raw_filename = os.path.join(raw_data_path, curr_date.strftime("%Y-%m-%d")+".csv")
+        all_state_data.to_csv(raw_filename, index=False)
+        gupload.upload(raw_filename, curr_date.strftime("%Y-%m-%d")+".csv",gdrive_fertilizer_raw_folder)
+
         all_state_data.columns = [re.sub('[^A-Za-z0-9]+', ' ',col).strip().replace(" ","_") for col in all_state_data.columns]
-        all_state_data['state_district_lower'] = all_state_data['State_Name'].str.lower().str.strip() + "_" + \
-                                                all_state_data['District_Name'].str.lower().str.strip()
+        all_state_data['Date'] = pd.to_datetime(all_state_data['Date'], format="%d-%m-%Y")
 
-        dist_summary = all_state_data.groupby(['State_Name','District_Name','state_district_lower','Date'],
-                                        as_index=False)['Quantity_Sold_MT','No_Of_Sale_Transaction'].sum()
-
-        dist_merged = pd.merge(dist_summary, state_codes_df, on='state_district_lower', how='left')
-        dist_merged.rename(columns={'Quantity_Sold_MT': 'Quantity_Sold_daily_district', 
-                                'No_Of_Sale_Transaction': 'Number_of_sale_Transactions_daily_district'},
-                                inplace=True)
-
-        historical_data = pd.read_csv(os.path.join(dir_path,'data_historical.csv'))
-        historical_data.rename(columns={'merge_name': 'state_district_lower'},inplace=True)
+        dist_summary = all_state_data.groupby(['State_Name','District_Name','Date'])[['Quantity_Sold_MT','No_Of_Sale_Transaction']].sum().reset_index()
+        dist_summary.columns = ['state_fert','district_fert','date','quantity_sold_daily_district','number_of_sale_transactions_daily_district']
+        dist_summary['merge_name'] = dist_summary['state_fert'].str.lower().str.strip() + "_" + \
+                                                    dist_summary['district_fert'].str.lower().str.strip()
+        dist_summary = dist_summary[~dist_summary['district_fert'].str.contains('^\d+$')].copy()
+        # display(dist_summary)
+        dist_merged = pd.merge(dist_summary, state_codes_df[['state_name','state_code','district_name','district_code','merge_name']], on='merge_name', how='left')
+        # display(dist_merged)
+    
+        hist_df = pd.read_csv(os.path.join(dir_path,'data_historical.csv'))
+        hist_df['merge_name'] = hist_df['state_fert'].str.lower().str.strip() + "_" + \
+                                                    hist_df['district_fert'].str.lower().str.strip()
+        hist_df['date'] = pd.to_datetime(hist_df['date'], format="%d-%m-%Y")
+        hist_df = hist_df[(hist_df['date'] < curr_date.strftime("%Y-%m-%d"))]
+        # display(hist_df)
+        stacked_df = pd.concat([hist_df, dist_merged],sort=False).reset_index(drop=True).drop_duplicates()
+        stacked_df = stacked_df.sort_values(by=['date','state_name','district_name'])
+    
+        final_df = stacked_df.copy()
+        stacked_df['date'] = stacked_df['date'].dt.strftime("%d-%m-%Y")
+        stacked_df.drop(columns='merge_name').to_csv(os.path.join(dir_path,'data_historical.csv'), index=False)
+        gupload.upload(os.path.join(dir_path,'data_historical.csv'), "data_historical.csv",gdrive_fertilizer_folder)
+        # display(stacked_df)
+    
+        final_df[['quantity_sold_roll_district', 
+                  'number_of_sale_transactions_roll_district']] = final_df.groupby('merge_name')[['quantity_sold_daily_district', 
+                                                            'number_of_sale_transactions_daily_district']].transform(lambda x: x.rolling(window=7,min_periods=1).mean())
+        state_df = final_df.groupby(['state_code','date'])[['quantity_sold_daily_district', 'number_of_sale_transactions_daily_district']].sum().reset_index()
+        state_df.columns = ['state_code', 'date', 'quantity_sold_daily_state', 'number_of_sale_transactions_daily_state']
+        state_df[['quantity_sold_roll_state', 
+                'number_of_sale_transactions_roll_state']] = state_df.groupby('state_code')[['quantity_sold_daily_state',
+                                                                'number_of_sale_transactions_daily_state']].transform(lambda x: x.rolling(window=7,min_periods=1).mean())
+        final_df = pd.merge(final_df, state_df, on=["state_code", "date"], how="left")
+    
+        india_df = final_df.groupby('date')[['quantity_sold_daily_district', 'number_of_sale_transactions_daily_district']].sum().reset_index()
+        india_df.columns = ['date', 'quantity_sold_daily_india', 'number_of_sale_transactions_daily_india']
+        india_df[['quantity_sold_roll_india', 'number_of_sale_transactions_roll_india']] = india_df[['quantity_sold_daily_india', 
+                                                                                           'number_of_sale_transactions_daily_india']].rolling(window=7,min_periods=1).mean()
+    
+        final_df = pd.merge(final_df, india_df, on="date", how="left")
+        daily_final_df = final_df[(final_df['date'] == curr_date.strftime("%Y-%m-%d"))].reset_index(drop=True)
+        daily_final_df['date'] = daily_final_df['date'].dt.strftime("%d-%m-%Y")
+        daily_final_df = daily_final_df[['date','state_name','state_code','district_name','district_code','quantity_sold_roll_district','number_of_sale_transactions_roll_district',
+                                    'quantity_sold_roll_state','number_of_sale_transactions_roll_state','quantity_sold_roll_india', 'number_of_sale_transactions_roll_india']]
+        filename = os.path.join(data_path, f"fertilizer_sales_daily_{curr_date.strftime('%d-%m-%Y')}.csv")
+        daily_final_df.to_csv(filename,index=False)
         
-        stacked_df = pd.concat([historical_data, dist_merged],sort=False).reset_index(drop=True)
-        stacked_df['Date'] = pd.to_datetime(stacked_df['Date'], format="%d-%m-%Y").dt.date
-        stacked_df.sort_values(by='Date', ascending=False)
-
-        stacked_df[['Quantity_Sold_roll_district',
-                'Number_of_sale_Transactions_roll_district']] = stacked_df.groupby('state_district_lower')[['Quantity_Sold_daily_district',
-                                                                        'Number_of_sale_Transactions_daily_district']].transform(
-                                                                        lambda x: x.rolling(7).mean()
-                                                                    )
-        state_df = stacked_df.groupby(['State_Name','Date'])[['Quantity_Sold_daily_district',
-                                                        'Number_of_sale_Transactions_daily_district']].sum().reset_index()
-        state_df.columns = ['State_Name', 'Date', 'Quantity_Sold_daily_state', 'Number_of_sale_Transactions_daily_state']
-        state_df[['Quantity_Sold_roll_state', 
-            'Number_of_sale_Transactions_roll_state']] = state_df.groupby('State_Name')[['Quantity_Sold_daily_state', 
-                                                                                        'Number_of_sale_Transactions_daily_state']
-                                                                                    ].transform(lambda x: x.rolling(7).mean())
-        
-        stacked_df = pd.merge(stacked_df, state_df, on=["State_Name", "Date"], how="left")
-        india_df = stacked_df.groupby('Date')[['Quantity_Sold_daily_district', 'Number_of_sale_Transactions_daily_district']].sum().reset_index()
-        india_df.columns = ['Date', 'Quantity_Sold_daily_india', 'Number_of_sale_Transactions_daily_india']
-        india_df[['Quantity_Sold_roll_india',
-            'Number_of_sale_Transactions_roll_india']] = india_df[['Quantity_Sold_daily_india', 
-                                                                'Number_of_sale_Transactions_daily_india']].rolling(7).mean()
-        stacked_df = pd.merge(stacked_df, india_df, on="Date", how="left")     
-        date_to_be_sent = currentDate                                          
-
-        final_df = stacked_df[stacked_df['Date']==date_to_be_sent.date()]
-        final_df = final_df[['State_Name', 'District_Name', 'Date', 'District_lgd',
-                'District_code', 'State_lgd', 'State_code',
-                'Quantity_Sold_daily_district',
-                'Number_of_sale_Transactions_daily_district',
-                'Quantity_Sold_roll_district',
-                'Number_of_sale_Transactions_roll_district',
-                'Quantity_Sold_daily_state', 'Number_of_sale_Transactions_daily_state',
-                'Quantity_Sold_roll_state', 'Number_of_sale_Transactions_roll_state',
-                'Quantity_Sold_daily_india', 'Number_of_sale_Transactions_daily_india',
-                'Quantity_Sold_roll_india', 'Number_of_sale_Transactions_roll_india']].reset_index(drop=True)
-        final_df['Date'] = pd.to_datetime(final_df['Date'])
-        final_df['Date'] = final_df['Date'].dt.strftime("%d-%m-%Y")
-
-
-        filename = os.path.join(data_path, currentDate.strftime("%d-%m-%Y")+'.csv')
-        final_df.to_csv(filename,index=False)
-        gupload.upload(filename, currentDate.strftime("%d-%m-%Y")+'.csv',gdrive_fertilizer_folder)
+        # filename = os.path.join(data_path, currentDate.strftime("%d-%m-%Y")+'.csv')
+        # final_df.to_csv(filename,index=False)
+        gupload.upload(filename, f"fertilizer_sales_daily_{curr_date.strftime('%d-%m-%Y')}.csv",gdrive_fertilizer_folder)
     
     except requests.exceptions.RequestException as e:
         print(e)
@@ -132,7 +133,7 @@ def read_fertilizer_data(**context):
 default_args = {
     'owner': 'airflow', 
     'depends_on_past': False,
-    'start_date': pendulum.datetime(year=2021, month=8, day=20, hour=12, minute=00 ).astimezone('Asia/Kolkata'),
+    'start_date': pendulum.datetime(year=2021, month=9, day=19, hour=12, minute=00 ).astimezone('Asia/Kolkata'),
     'provide_context': True,
     'email': ['gursharan_singh@isb.edu'],
     'email_on_failure': True,
